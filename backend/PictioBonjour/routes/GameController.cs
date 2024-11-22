d49166ea7a12012bb1c62a9bc21bf2e56598c67e
rayan-ad <159818891+rayan-ad@users.noreply.github.com>
2024-11-22 22:12:48 +0100

    added game controller

---
+++ backend/PictioBonjour/routes/GameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PictioBonjour.routes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet("startGame")]
+        public IActionResult StartGame()
+        {
+
+            return Ok();
+        }
+    }
+}
