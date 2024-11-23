efe466ea19057d61beff722fc74f2a7d628ee00e
rayan-ad <159818891+rayan-ad@users.noreply.github.com>
2024-11-23 02:30:05 +0100

    Added EmojisGenerator+EmojisFile

---
+++ backend/PictioBonjour/services/EmojieGeneratorService.cs
@@ -0,0 +1,77 @@
+﻿namespace PictioBonjour.services
+{
+    public class EmojieGeneratorService
+    {
+        private List<string> emojis = new List<string>();
+        private string randomTargetEmojie;
+        public EmojieGeneratorService()
+        {
+            addEmojisToListEmjis();
+            GenerateTargetEmoji();
+            GeneratePotentialEmoji();
+          
+        }
+
+        private void addEmojisToListEmjis()
+        {
+            string path = "Ressource\\Emojis.txt";
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+
+                            emojis.Add(s);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fichier introuvable");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public string GenerateTargetEmoji()
+        {
+            if (emojis == null || emojis.Count == 0)
+            {
+                throw new InvalidOperationException("La liste des emojis est vide ou nulle.");
+            }
+            Random targetEmojieRand = new Random();
+            int randomIndex = targetEmojieRand.Next(emojis.Count);
+            randomTargetEmojie = emojis[randomIndex];
+             return randomTargetEmojie;
+        }
+        public List<string> GeneratePotentialEmoji()
+        {
+            
+            if (emojis == null || emojis.Count == 0)
+            {
+                throw new InvalidOperationException("La liste des emojis est vide ou nulle.");
+            }
+
+            HashSet<string> uniqueEmojis = new HashSet<string> { randomTargetEmojie };
+            Random random = new Random();
+
+            while (uniqueEmojis.Count < 6) // 1 cible + 5 options = 6 au total
+            {
+                int randomIndex = random.Next(emojis.Count);
+                uniqueEmojis.Add(emojis[randomIndex]);
+            }
+
+            return uniqueEmojis.ToList();
+
+        }
+    }
+}
+
