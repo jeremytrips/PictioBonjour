6c10154afafbcefd420e8a55b1b0c8f7971c7231
Hadje <hadje.abakar@student.vinci.be>
2024-11-23 23:05:25 +0100

    update:css emoji

--- pictio-bonjour-front/routing/src/components/App.tsx
+++ pictio-bonjour-front/routing/src/components/App.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from "react";
-import { HubConnection, HubConnectionBuilder, HubConnectionState, LogLevel } from '@microsoft/signalr'
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  HubConnectionState,
+  LogLevel,
+} from "@microsoft/signalr";
 import "./App.css";
 import PlayButton from "./PlayButton";
 import Painter from "./Painter";
 import Paint from "./Paint";
 import Guesser from "./Guesser";
 import { parse } from "uuid";
+import { div, p } from "framer-motion/client";
 
 enum States {
   Ready = "ready",
@@ -15,113 +21,134 @@ enum States {
 
 export enum UserState {
   Drawer,
-  Player
+  Player,
 }
-
 
 function App() {
   const [currentState, setCurrentState] = useState(States.Ready);
-  const [connection, setConnection] = useState<HubConnection | null>(null)
-  const [playersNumber, setPlayersNumber] = useState(0)
+  const [connection, setConnection] = useState<HubConnection | null>(null);
+  const [playersNumber, setPlayersNumber] = useState(0);
   const [userState, SetUserState] = useState<UserState | null>(null);
   const [target_emojis, setTargetEmoji] = useState("");
   const [potential_emojis, setPotentialEmoji] = useState<string[]>([]);
   const connectionRef = useRef<HubConnection | null>(null);
+  const hexCode = target_emojis.split("U+")[1]; // Extraire la partie hexadécimale du code
 
-  
   window.onbeforeunload = function () {
-    connectionRef.current?.invoke("LeaveGame")
-      .then(() => console.log("left"))
-  }
+    connectionRef.current?.invoke("LeaveGame").then(() => console.log("left"));
+  };
 
   useEffect(() => {
     connectionRef.current = new HubConnectionBuilder()
       .withUrl("http://localhost:5095/hub/game")
       .build();
 
-    connectionRef.current.start()
-      .then(() => {
-        connectionRef.current!.invoke("JoinGame")
-      });
-      connectionRef.current.on("onStatusChanged", (state)=>{
-        SetUserState(state)
-      })
+    connectionRef.current.start().then(() => {
+      connectionRef.current!.invoke("JoinGame");
+    });
+    connectionRef.current.on("onStatusChanged", (state) => {
+      SetUserState(state);
+    });
 
-    connectionRef.current.on("ReceivePotentialEmojis", (data)=>{
-      setPotentialEmoji(data)
-      setCurrentState(States.Playing)
+    connectionRef.current.on("ReceivePotentialEmojis", (data) => {
+      setPotentialEmoji(data);
+      setCurrentState(States.Playing);
+    });
 
-
-    })
-
-    connectionRef.current.on("ReceiveTargetEmojis", (data)=>{
-      setTargetEmoji(data)
-      setCurrentState(States.Playing)
-
-    })
-
+    connectionRef.current.on("ReceiveTargetEmojis", (data) => {
+      setTargetEmoji(data);
+      setCurrentState(States.Playing);
+    });
 
     connectionRef.current.on("onStatusChanged", (state) => {
-      SetUserState(state)
+      SetUserState(state);
     });
     if (userState !== UserState.Drawer) {
       connectionRef.current.on("onCanvasDrawed", (word) => {
         // setEmojis(word)
       });
-
     }
 
     return () => {
-      connectionRef.current?.invoke("LeaveGame")
-      console.log("unmounting")
-    }
-  }, [])
-
+      connectionRef.current?.invoke("LeaveGame");
+      console.log("unmounting");
+    };
+  }, []);
 
   const play = () => {
-
-    connectionRef.current?.invoke("OnGameStarter")
-  }
+    connectionRef.current?.invoke("OnGameStarter");
+  };
 
   function renderComponent(state: States) {
-
     switch (state) {
       case States.Ready:
         return (
           <>
             <p className="title">less is more</p>
-            <div style={{  }}>
-              {
-                userState === 0 ? <PlayButton onClick={play} /> : ""
-              }
+            <div style={{}}>
+              {userState === 0 ? <PlayButton onClick={play} /> : ""}
             </div>
           </>
         );
       case States.Playing:
-
-        return <>
-          {
-
-            <div className="container" style={{display: 'flex', flexDirection: 'row'}}>
-              <Paint connection={connectionRef.current!} userState={userState!} />
-            </div>
-          }
-
-        </>
-
+        return (
+          <>
+            {
+              <div
+                className="container"
+                style={{ display: "flex", flexDirection: "row" }}
+              >
+                <Paint
+                  connection={connectionRef.current!}
+                  userState={userState!}
+                />
+              </div>
+            }
+          </>
+        );
 
       default:
         return null;
     }
-  };
-
+  }
 
   return (
-    <div className="container" style={{ display: 'flex', flexDirection: 'row' }}>
+    <div
+      className="container emoji"
+      
+    >
+      {userState === UserState.Drawer && (
+        <>
+          {hexCode && (
+            <div style={{ fontSize: "5em" }}>
+              {String.fromCodePoint(parseInt(hexCode, 16))}
+            </div>
+          )}
+        </>
+      )}
+      {/* Si l'utilisateur est un Guesser, affiche les emojis potentiels */}
+      {userState === UserState.Player && potential_emojis && (
+        <div
+        
+        className="emoji-container"
+          style={{
+            display: "flex",
+            gap: "10px",
+            flexWrap: "nowrap",
+            justifyContent: "center",
+          }}
+        >
+          {potential_emojis.map((emoji, index) => (
+            <div  key={index} style={{ fontSize: "2em", margin: "0" }}>
+              {String.fromCodePoint(parseInt(emoji.split("U+")[1], 16))}
+            </div>
+          ))}
+        </div>
+      )}
+
       {renderComponent(currentState)}
     </div>
   );
-
 }
 
 export default App;
